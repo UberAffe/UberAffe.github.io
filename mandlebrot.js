bd73e1a7f856aae430e4ffa57c28dab64d470e45
UberAffe <mattkeran@gmail.com>
2018-01-22 12:04:18 -0600

    test

--- mandlebrot.js
+++ mandlebrot.js
@@ -22,9 +22,9 @@ function setup() {
 
       var pix = (x + y * width) * 4;
       pixels[pix+0] = bright;
-      pixels[pix+1] = 51;
-      pixels[pix+3] = 51;
-      pixels[pix+4] = 255;
+      //pixels[pix+1] = 51;
+      //pixels[pix+3] = 51;
+      //pixels[pix+4] = 255;
     }
   }
   updatePixels();
